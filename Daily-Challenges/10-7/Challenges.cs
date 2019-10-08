5b2b97fb9f6d985de9c17e2474c9a6f34fad32db
Zach Pfeifer <mr.zman1@yahoo.com>
2019-10-08 13:36:45 -0600

    10/8 challenge

--- Daily-Challenges/10-7/Challenges.cs
+++ Daily-Challenges/10-7/Challenges.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Challenges
 {
@@ -56,6 +58,56 @@ namespace Challenges
       }
       return sum;
     }
+
+
+    //Write a function that will loop over an array and group all similar values in sub arrays Example group([1,1,3,4,5,4]) => output [[1,1],[3],[4,4],[5]
+    public static List<List<int>> ArrayGroup(int[] arr)
+    {
+      //Create a Dictionary
+      Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+      //Loop over Array and add each value as a key if it doesnt exist
+      foreach (int val in arr)
+      {
+        //If it doesnt exist
+        if (dict.ContainsKey(val))
+        {
+          //add new list
+          dict.Add(val, new List<int>());
+        }
+        //push into an array at that key
+        dict[val].Add(val);
+      }
+      //Return Dictionary Values as an Array
+      return dict.Values.ToList();
+    }
+
+
+    //Take 2 on Arraygroup
+    public static int[][] ArrayGroup2(int[] arr)
+    {
+      //Create a Dictionary
+      Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+      //Loop over Array and add each value as a key if it doesnt exist
+      foreach (int val in arr)
+      {
+        //If it doesnt exist
+        if (dict.ContainsKey(val))
+        {
+          //add new list
+          dict.Add(val, new List<int>());
+        }
+        //push into an array at that key
+        dict[val].Add(val);
+      }
+      //Return Dictionary Values as an Array
+      var values = dict.Values.ToList();
+      var inList = new List<int[]>();
+      values.ForEach(v => inList.Add(v.ToArray()));
+      return inList.ToArray();
+    }
+
+
+
 
   }
 }
--- Program.cs
+++ Program.cs
@@ -39,7 +39,15 @@ namespace Challenges
       System.Console.WriteLine(Challenges.arraySum(new int[] { 10, 10, 10, 10, 10 }) + " = 50?");
 
 
+      Console.WriteLine(" ");
+      Console.WriteLine("Loop over an Array and Group all similar values?");
+      // //Write a function that will loop over an array and group all similar values in sub arrays Example group([1,1,3,4,5,4]) => output [[1,1],[3],[4,4],[5]
+      System.Console.WriteLine(Challenges.ArrayGroup(new int[] { 1, 1, 3, 4, 5, 4 }) + " = [[1,1],[3],[4,4],[5]?");
 
+      Console.WriteLine(" ");
+      Console.WriteLine("Loop over an Array and Group all similar values? Take 2");
+      // //Write a function that will loop over an array and group all similar values in sub arrays Example group([1,1,3,4,5,4]) => output [[1,1],[3],[4,4],[5]
+      System.Console.WriteLine(Challenges.ArrayGroup2(new int[] { 1, 1, 3, 4, 5, 4 }) + " = [[1,1],[3],[4,4],[5]?");
 
 
 
